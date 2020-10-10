9c218a4e90791c16d23445bb19334b54f9b5caca
Matthew Skinner <matt@pcmus.com>
2020-10-10 14:31:07 +1100

    Update to V3.x
    Signed-off-by: Matthew Skinner <matt@pcmus.com>

--- src/main/java/org/openhab/binding/espmilighthub/EspMilightHubBindingConstants.java
+++ src/main/java/org/openhab/binding/espmilighthub/EspMilightHubBindingConstants.java
@@ -14,7 +14,7 @@
 package org.openhab.binding.espmilighthub;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.eclipse.smarthome.core.thing.ThingTypeUID;
+import org.openhab.core.thing.ThingTypeUID;
 
 /**
  * The {@link EspMilightHubBindingConstants} class defines common constants, which are
--- src/main/java/org/openhab/binding/espmilighthub/internal/EspMilightHubHandlerFactory.java
+++ src/main/java/org/openhab/binding/espmilighthub/internal/EspMilightHubHandlerFactory.java
@@ -15,12 +15,12 @@ package org.openhab.binding.espmilighthub.internal;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
-import org.eclipse.smarthome.core.thing.Bridge;
-import org.eclipse.smarthome.core.thing.Thing;
-import org.eclipse.smarthome.core.thing.ThingTypeUID;
-import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
-import org.eclipse.smarthome.core.thing.binding.ThingHandler;
-import org.eclipse.smarthome.core.thing.binding.ThingHandlerFactory;
+import org.openhab.core.thing.Bridge;
+import org.openhab.core.thing.Thing;
+import org.openhab.core.thing.ThingTypeUID;
+import org.openhab.core.thing.binding.BaseThingHandlerFactory;
+import org.openhab.core.thing.binding.ThingHandler;
+import org.openhab.core.thing.binding.ThingHandlerFactory;
 import org.openhab.binding.espmilighthub.handler.EspMilightHubBridgeHandler;
 import org.openhab.binding.espmilighthub.handler.EspMilightHubHandler;
 import org.osgi.service.component.annotations.Component;
